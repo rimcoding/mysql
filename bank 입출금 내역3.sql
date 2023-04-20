3c8c12a7bac3d2edf12638e5d015fe68a77b07c6
yog4130 <yog4130@gamil.com>
2023-04-20 12:12:51 +0900

    입출금 쿼리 최종

--- bank 입출금 내역3.sql
+++ bank 입출금 내역3.sql
@@ -5,21 +5,6 @@ SELECT *
 FROM history_tb 
 WHERE w_account_id = 2 or d_account_id = 2;
 
-
--- 입금 내역
-SELECT h.id,h.amount,h.d_balance,h.created_at,
-       da.number AS receiver,
-       ifnull(h.w_account_id,'ATM') as sender,
-       wa.number
-FROM history_tb AS h
-LEFT JOIN account_tb AS da
-ON h.d_account_id = da.id
-LEFT JOIN account_tb AS wa
-ON h.w_account_id = wa.id
-WHERE h.d_account_id = 1 or h.w_account_id = 1;
-
-
-
 -- 입금 내역 : 쿼리 결정
 SELECT h.id,h.amount,h.d_balance AS balance,h.created_at,
        ifnull(wa.number,'ATM') AS sender,
@@ -42,6 +27,21 @@ LEFT JOIN account_tb AS da
 ON h.d_account_id = da.id
 WHERE h.w_account_id = 1;
 
+-- 입출금 쿼리 결정
+-- CASE WHEN THEN END
 
+SELECT h.id,h.amount,
+CASE WHEN h.w_account_id = 1 THEN (h.w_balance)
+     WHEN h.d_account_id = 1 THEN (h.d_balance)
+     END AS balance,
+     ifnull(wa.number,'ATM') AS sender,
+     ifnull(da.number,'ATM') AS receiver,
+     h.created_at
+FROM history_tb AS h
+LEFT JOIN account_tb AS da
+ON h.d_account_id = da.id
+LEFT JOIN account_tb AS wa
+ON h.w_account_id = wa.id
+WHERE h.d_account_id = 1 OR h.w_account_id = 1;
 
 
